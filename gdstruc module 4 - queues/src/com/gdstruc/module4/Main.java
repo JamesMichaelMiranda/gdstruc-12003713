0299249bc4bf0b857d970652c9948135a57f91fa
JamesMichaelMiranda <miranda.james1357@gmail.com>
2021-12-02 14:49:54 +0800

    changed module 4 queues

--- gdstruc module 4 - queues/src/com/gdstruc/module4/Main.java
+++ gdstruc module 4 - queues/src/com/gdstruc/module4/Main.java
@@ -6,21 +6,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        int min = 1;
-        int max = 7;
-        int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
-
-        Scanner scanner = new Scanner(System.in);
-        String enterkey = scanner.nextLine();
-
-        if(enterkey.equals("")) {
-
-            System.out.println(random_int + " players have entered the queue");
-        }
-
-
-
         ArrayQueue queue = new ArrayQueue(7);
 
         queue.add(new Player(1,"Nox", 20));
@@ -29,9 +14,18 @@ public class Main {
         queue.add(new Player(4, "JT", 78));
         queue.add(new Player(5, "Lymtex", 80));
 
+        int min = 1;
+        int max = 5;
+        int randomNumber = (int)Math.floor(Math.random()*(max-min+1)+min);
 
-        queue.printQueue();
 
+        if (randomNumber != 5) {
+            System.out.println("You have not entered Queue");
+        }
 
+        else if (randomNumber == 5) {
+            System.out.println(randomNumber + " players have entered the queue");
+            queue.printQueue();
+        }
     }
 }
